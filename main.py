ba65fe5f5cfdea4aed96da2dba1f29e3a6b612c4
Fernando Viton <fcode@viton.org>
2024-11-29 21:57:03 -0800

    Refactor main, end when column full

--- main.py
+++ main.py
@@ -11,27 +11,42 @@ def get_valid_column_from_user(board):
             print(f"Please choose a column that is not full")
         else:
             return column
+        
+# Check if we have a winner
+# column_hint is the column that was last played (which has to be part of the winning condition)
+def have_winner(board, player_value, column_hint):
+    # for now we end only when a column is full
+    for column in range(board.columns):
+        if board.is_column_full(column):
+            return True
+    return False
+        
+def do_player_turn(board, player_value):
+    column = get_valid_column_from_user(board)
+    board.add_to_column(column, player_value)
+    if have_winner(board, player_value, column):
+        return True
+    
+def do_computer_turn(board, computer_value):
+    # column = next_move(board, computer_value)
+    column = 0 # just pick 0 always for now
+    board.add_to_column(column, computer_value)
+    if have_winner(board, computer_value, column):
+        return True
 
 def game_loop(board):
     player_value=1
     computer_value=2
-    turn=0
     while True:
         print(board)
-        turn += 1
 
-        if turn > 4:
-            print("Game over!")
+        if do_player_turn(board, player_value):
+            print("End of game, player played last move")
             break
 
-        # Player turn
-        player_column = get_valid_column_from_user(board)
-        board.add_to_column(player_column, player_value)
-
-        # Computer turn
-        # computer_column = next_move(board, computer_value)
-        computer_column = 0 # just pick 0 always for now
-        board.add_to_column(computer_column, computer_value)
+        if do_computer_turn(board, computer_value):
+            print("End of game, computer played last move")
+            break
 
 if __name__ == "__main__":
     board = Connect4Board(rows=6, columns=7)
