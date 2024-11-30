9dd67d018512145df904d10f5f9b93bb1b63ea5d
Nichol Viton <nichol@viton.org>
2024-11-29 21:13:35 -0800

    connected turns to board

--- turns.py
+++ turns.py
@@ -1,43 +1,52 @@
+from board import Connect4Board
+
 GAME_SIZE = 10
 
 def create_empty_matrix(size):
     return [[0 for _ in range(size)] for _ in range(size)]
 
 
-def my_turn(matrix):
+def my_turn(board):
     column = int(input("Please enter a number: "))
     print(f"You entered: {column}")
-    #if (matrix[0][0] == 0)
     row = 0
-    for row in range(len(matrix)):
-        if matrix[row][column] != 0: 
+    for row in range(board.rows):
+        if board.get_cell(row, column) != 0: 
             row = row-1
-            break;        
+            break
     if row > 0:
-        matrix[row][int(column)] = 1
+        board.set_cell(row, column, 1)
 
-def check_for_winner(matrix, row, column, number):
-    for row in range(len(matrix)):
+def check_for_winner(board, row, column, number):
+    for row in range(board.rows):
         pass
     
     return True
 
-def take_turns(matrix):
+def take_turns(board):
     while True: 
-        my_turn(matrix)
-        my_turn(matrix)
-        my_turn(matrix)
-        my_turn(matrix)
-        if check_for_winner(matrix, 0, 0, 0) == True:
+        my_turn(board)
+        my_turn(board)
+        my_turn(board)
+        my_turn(board)
+        if check_for_winner(board, 0, 0, 0) == True:
             print(f"You win!")
             break
 
-# Example usage
+def print_board(board):
+    for row in range(board.rows):
+        print(f"")
+        for column in range(board.columns):
+            value = board.get_cell(row, column)
+            print(f" {value}", end=' ')
+
 
 if __name__ == "__main__":
-    matrix = create_empty_matrix(GAME_SIZE)
-    take_turns(matrix)
+    board = Connect4Board(rows=6, columns=7)
+    #matrix = create_empty_matrix(GAME_SIZE)
+    take_turns(board)
+    print_board(board)
 
-    for row in matrix:
-        print(row)
+    #for row in matrix:
+    #    print(row)
 
